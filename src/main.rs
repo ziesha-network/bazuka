531e5fa3e4673ce653a8235a1734ab7887cd7498
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2021-12-16 15:45:22 +0330

    Implement p2p ping example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use futures::executor::block_on;
+use futures::prelude::*;
+use libp2p::ping::{Ping, PingConfig};
+use libp2p::swarm::{Swarm, SwarmEvent, dial_opts::DialOpts};
+use libp2p::{identity, Multiaddr, PeerId};
+use std::error::Error;
+use std::task::Poll;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    let transport = block_on(libp2p::development_transport(local_key))?;
+
+    // Create a ping network behaviour.
+    //
+    // For illustrative purposes, the ping protocol is configured to
+    // keep the connection alive, so a continuous sequence of pings
+    // can be observed.
+    let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
+
+    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {}", addr)
+    }
+
+    block_on(future::poll_fn(move |cx| loop {
+        match swarm.poll_next_unpin(cx) {
+            Poll::Ready(Some(event)) => match event {
+                SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
+                SwarmEvent::Behaviour(event) => println!("{:?}", event),
+                _ => {}
+            },
+            Poll::Ready(None) => return Poll::Ready(()),
+            Poll::Pending => return Poll::Pending
+        }
+    }));
+
+    Ok(())
 }
