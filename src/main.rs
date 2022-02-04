477c1b2413370e10cfeb8e612a356989b4e6978b
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-02-04 20:07:52 +0330

    Init async http client/server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello Bazuka!");
+use futures::join;
+use tokio::time::{sleep, Duration};
+use warp::Filter;
+
+async fn heartbeat() {
+    loop {
+        println!("Lub dub!");
+        sleep(Duration::from_millis(1000)).await;
+        let resp = reqwest::get("http://127.0.0.1:3030/hello/bazuka")
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+        println!("{}", resp);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    let http_future = warp::serve(hello).run(([127, 0, 0, 1], 3030));
+
+    let heartbeat_future = heartbeat();
+
+    join!(http_future, heartbeat_future);
 }
