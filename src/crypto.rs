34dc76d6ab850d321d634a4f7eaedcb1da0f088c
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-02-06 19:09:43 +0330

    Add bn254 Fr elements

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,3 +1,5 @@
+use ff::PrimeField;
+
 pub trait SignatureScheme<Pub, Priv, Sig> {
     fn generate() -> (Pub, Priv);
     fn sign(sk: Priv, msg: Vec<u8>) -> Sig;
@@ -9,3 +11,11 @@ pub trait VerifiableRandomFunction<Pub, Priv, Output, Proof> {
     fn evaluate(sk: Priv, input: Vec<u8>) -> (Output, Proof);
     fn verify(pk: Pub, input: Vec<u8>, output: Output, proof: Proof) -> bool;
 }
+
+#[derive(PrimeField)]
+#[PrimeFieldModulus = "21888242871839275222246405745257275088548364400416034343698204186575808495617"]
+#[PrimeFieldGenerator = "7"]
+#[PrimeFieldReprEndianness = "little"]
+struct Fr([u64; 4]);
+
+struct PointAffine(Fr, Fr);
