d55bc0862ed246d11f7613ff30fd58fc10f5686e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-02-07 02:08:40 +0330

    Prototype mimc hash func

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,6 @@
 use ff::{Field, PrimeField};
-use std::ops::Neg;
+use sha3::{Digest, Sha3_256};
+use std::ops::*;
 
 pub trait SignatureScheme<Pub, Priv, Sig> {
     fn generate() -> (Pub, Priv);
@@ -17,16 +18,12 @@ pub trait VerifiableRandomFunction<Pub, Priv, Output, Proof> {
 #[PrimeFieldModulus = "21888242871839275222246405745257275088548364400416034343698204186575808495617"]
 #[PrimeFieldGenerator = "7"]
 #[PrimeFieldReprEndianness = "little"]
-struct Fr([u64; 4]);
+pub struct Fr([u64; 4]);
 
-struct PointAffine(Fr, Fr);
+pub struct PointAffine(Fr, Fr);
 
 lazy_static! {
-    static ref A: Fr = {
-        let mut a = Fr::one();
-        a.neg();
-        a
-    };
+    static ref A: Fr = Fr::one().neg();
     static ref D: Fr = Fr::from_str_vartime(
         "12181644023421730124874158521699555681764249180949974110617291017600649128846"
     )
@@ -41,4 +38,33 @@ lazy_static! {
         )
         .unwrap()
     );
+}
+
+pub struct MiMC {
+    params: Vec<Fr>,
+}
+
+impl MiMC {
+    pub fn new(seed: &[u8]) -> MiMC {
+        let mut hasher = Sha3_256::new();
+        hasher.update(seed);
+        let _result = hasher.finalize();
+        MiMC { params: Vec::new() }
+    }
+    fn encrypt(&self, mut inp: Fr, k: &Fr) -> Fr {
+        for c in self.params.iter() {
+            let tmp = inp + c + k;
+            inp = tmp * tmp;
+            inp.mul_assign(&tmp);
+        }
+        inp.add_assign(k);
+        inp
+    }
+    pub fn hash(&self, data: &Vec<Fr>) -> Fr {
+        let mut digest = Fr::zero();
+        for d in data.iter() {
+            digest.add_assign(&self.encrypt(d.clone(), &digest));
+        }
+        digest
+    }
 }
