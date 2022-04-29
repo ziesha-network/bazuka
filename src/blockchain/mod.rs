ea2c6df27b7116cd7939a6753b944d716c8d8a8c
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-04-29 20:21:01 +0430

    Num blocks in between is one less than recalc interval

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -104,7 +104,7 @@ impl<K: KvStore> KvStoreChain<K> {
                 .header;
             let time_delta =
                 last_block.proof_of_work.timestamp - prev_block.proof_of_work.timestamp;
-            let avg_block_time = time_delta / config::DIFFICULTY_CALC_INTERVAL as u32;
+            let avg_block_time = time_delta / (config::DIFFICULTY_CALC_INTERVAL - 1) as u32;
             let diff_change =
                 (config::BLOCK_TIME as f32 / avg_block_time as f32).clamp(0.5f32, 2f32);
             let new_diff =
