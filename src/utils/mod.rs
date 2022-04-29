84b5c8a7253a6918f3457d547ff14d57eb7725e8
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-04-29 19:53:46 +0430

    Timestamp is u32

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,11 +4,11 @@ pub use decode::*;
 
 mod decode;
 
-pub fn local_timestamp() -> u64 {
+pub fn local_timestamp() -> u32 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards")
-        .as_secs()
+        .as_secs() as u32
 }
 
 pub fn median<T: Clone>(inps: &Vec<T>) -> T {
