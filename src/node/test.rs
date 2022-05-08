6fdcbaa02e37cd3ab460dcf5644ffd5cc13c3896
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-08 21:06:38 +0430

    Send request to simulated node

--- src/node/test.rs
+++ src/node/test.rs
@@ -1,12 +1,13 @@
 use super::*;
 
+use super::api::messages::*;
 use crate::blockchain::KvStoreChain;
 use crate::config::genesis;
 use crate::db::RamKvStore;
 
 struct Node {
     addr: PeerAddress,
-    incoming: Arc<mpsc::UnboundedSender<IncomingRequest>>,
+    incoming: SenderWrapper,
     outgoing: mpsc::UnboundedReceiver<OutgoingRequest>,
 }
 
@@ -22,7 +23,9 @@ fn create_test_node(
         node,
         Node {
             addr,
-            incoming: Arc::new(inc_send),
+            incoming: SenderWrapper {
+                chan: Arc::new(inc_send),
+            },
             outgoing: out_recv,
         },
     )
@@ -30,7 +33,7 @@ fn create_test_node(
 
 async fn route(
     mut outgoing: mpsc::UnboundedReceiver<OutgoingRequest>,
-    incs: HashMap<PeerAddress, Arc<mpsc::UnboundedSender<IncomingRequest>>>,
+    incs: HashMap<PeerAddress, SenderWrapper>,
 ) -> Result<(), NodeError> {
     loop {
         if let Some(req) = outgoing.recv().await {
@@ -50,6 +53,7 @@ async fn route(
                 resp: resp_snd,
             };
             incs[&s]
+                .chan
                 .send(inc_req)
                 .map_err(|_| NodeError::NotListeningError)?;
             req.resp
@@ -64,12 +68,52 @@ async fn route(
     Ok(())
 }
 
+#[derive(Clone)]
+struct SenderWrapper {
+    chan: Arc<mpsc::UnboundedSender<IncomingRequest>>,
+}
+
+impl SenderWrapper {
+    pub async fn raw(&self, body: Request<Body>) -> Result<Body, NodeError> {
+        let (resp_snd, mut resp_rcv) = mpsc::channel::<Result<Response<Body>, NodeError>>(1);
+        let req = IncomingRequest {
+            socket_addr: "0.0.0.0:0".parse().unwrap(),
+            body,
+            resp: resp_snd,
+        };
+        self.chan
+            .send(req)
+            .map_err(|_| NodeError::NotListeningError)?;
+
+        let body = resp_rcv
+            .recv()
+            .await
+            .ok_or(NodeError::NotAnsweringError)??
+            .into_body();
+
+        Ok(body)
+    }
+    async fn json_get<Req: serde::Serialize, Resp: serde::de::DeserializeOwned>(
+        &self,
+        addr: String,
+        req: Req,
+    ) -> Result<Resp, NodeError> {
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri(format!("{}?{}", addr, serde_qs::to_string(&req)?))
+            .body(Body::empty())?;
+        let body = self.raw(req).await?;
+        let resp: Resp = serde_json::from_slice(&hyper::body::to_bytes(body).await?)?;
+        Ok(resp)
+    }
+}
+
 fn test_network(
     num_nodes: usize,
 ) -> (
     impl futures::Future,
     impl futures::Future,
-    HashMap<PeerAddress, Arc<mpsc::UnboundedSender<IncomingRequest>>>,
+    HashMap<PeerAddress, SenderWrapper>,
 ) {
     let addresses: Vec<PeerAddress> = (0..num_nodes)
         .map(|i| PeerAddress(format!("127.0.0.1:{}", 3030 + i).parse().unwrap()))
@@ -78,10 +122,7 @@ fn test_network(
         .iter()
         .map(|p| create_test_node(*p, addresses.clone()))
         .unzip();
-    let incs: HashMap<_, _> = nodes
-        .iter()
-        .map(|n| (n.addr, Arc::clone(&n.incoming)))
-        .collect();
+    let incs: HashMap<_, _> = nodes.iter().map(|n| (n.addr, n.incoming.clone())).collect();
     let route_futs = nodes
         .into_iter()
         .map(|n| route(n.outgoing, incs.clone()))
@@ -100,6 +141,13 @@ async fn test_node() {
     let test_logic = async {
         tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
         println!("Start test logic...");
+        let resp = chans[&PeerAddress("127.0.0.1:3030".to_string().parse().unwrap())]
+            .json_get::<GetStatsRequest, GetStatsResponse>(
+                "http://127.0.0.1:3030/stats".to_string(),
+                GetStatsRequest {},
+            )
+            .await;
+        println!("Stats: {:?}", resp);
     };
     tokio::join!(node_futs, route_futs, test_logic);
 }
