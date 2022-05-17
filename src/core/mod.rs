afe9490c00db15d28a0ffc4a728f6fc61541e865
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-17 13:24:02 +0430

    Remove unnecessarry code

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,8 +4,6 @@ pub mod hash;
 mod header;
 mod transaction;
 
-use std::fmt::Debug;
-
 use crate::crypto;
 
 pub type Money = u64;
@@ -20,36 +18,3 @@ pub type Header = header::Header<Hasher>;
 pub type Block = blocks::Block<Hasher, Signer>;
 
 pub type ProofOfWork = header::ProofOfWork;
-
-macro_rules! auto_trait {
-    (
-        $(
-            $(#[$doc:meta])+
-            trait $name:ident: $( $bound:path ),+;
-        )+
-    ) => {
-        $(
-            $(#[$doc])+
-            pub trait $name: $( $bound + )+ {}
-            impl <T: $($bound +)+> $name for T {}
-        )+
-    };
-}
-
-auto_trait!(
-    /// A type that implements Serialize in node runtime
-    trait AutoSerialize: serde::ser::Serialize;
-
-    /// A type that implements Deserialize in node runtime
-    trait AutoDeserialize: serde::de::DeserializeOwned;
-    /// A type that implements Hash in node runtime
-    trait AutoHash: core::hash::Hash;
-    /// A type that implements Display in runtime
-    trait AutoDisplay: core::fmt::Display;
-    /// A type that implements CanBe32Bits
-    trait CanBe32Bits: core::convert::From<u32>;
-);
-
-/// A type that can be used at runtime
-pub trait MemberBound: Send + Sync + Sized + Debug + Clone + Eq + PartialEq + 'static {}
-impl<T: Send + Sync + Sized + Debug + Clone + Eq + PartialEq + 'static> MemberBound for T {}
