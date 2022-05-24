eb11707e7dea9661aaf414d52fd8cdf37c42520b
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-24 15:45:37 +0430

    Remove LENGTH from Hash trait

--- src/core/hash.rs
+++ src/core/hash.rs
@@ -4,9 +4,6 @@ use sha3::{Digest, Sha3_256};
 use std::fmt::Debug;
 
 pub trait Hash: Debug + Clone + Serialize + 'static {
-    /// The length in bytes of the Hasher output
-    const LENGTH: usize;
-
     type Output: Debug
         + Serialize
         + DeserializeOwned
@@ -24,8 +21,6 @@ pub trait Hash: Debug + Clone + Serialize + 'static {
 pub struct Sha3Hasher;
 
 impl Hash for Sha3Hasher {
-    const LENGTH: usize = 32;
-    // U32 is copy from the macro named impl_sha3 in RustCrypto
     type Output = [u8; 32];
 
     fn hash(s: &[u8]) -> Self::Output {
