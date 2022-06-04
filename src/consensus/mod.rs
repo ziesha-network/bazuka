262a3f40a766cb0322ecedfe0088be882494df64
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-06-04 16:01:08 +0430

    Cache randomx hashers

--- src/consensus/mod.rs
+++ src/consensus/mod.rs
@@ -1,17 +1,20 @@
 pub mod pow {
     use rust_randomx::{Context, Hasher, Output};
+    use std::collections::HashMap;
     use std::sync::{Arc, Mutex};
 
     lazy_static! {
-        pub static ref HASHER: Arc<Mutex<Option<Hasher>>> = Arc::new(Mutex::new(None));
+        pub static ref HASHER: Arc<Mutex<HashMap<Vec<u8>, Hasher>>> =
+            Arc::new(Mutex::new(HashMap::new()));
     }
 
     pub fn hash(key: &[u8], input: &[u8]) -> Output {
+        // TODO: Should not keep all of hashers!
         let mut hasher = HASHER.lock().unwrap();
-        if hasher.is_none() || hasher.as_ref().unwrap().context().key() != key {
-            log::info!("Initializing RandomX hasher...");
-            *hasher = Some(Hasher::new(Arc::new(Context::new(key, false))));
-        }
-        hasher.as_ref().unwrap().hash(input)
+        let key = key.to_vec();
+        hasher
+            .entry(key.clone())
+            .or_insert_with(|| Hasher::new(Arc::new(Context::new(&key, false))))
+            .hash(input)
     }
 }
