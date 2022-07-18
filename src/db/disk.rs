f56d410a0d21bbdddaec960f0782ff692712ae65
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-07-18 19:59:26 +0430

    Provide snapshots of the readonly database

--- src/db/disk.rs
+++ src/db/disk.rs
@@ -2,7 +2,10 @@ use super::*;
 use leveldb::batch::Batch;
 use leveldb::database::batch::Writebatch;
 use leveldb::database::cache::Cache;
-use leveldb::database::Database;
+use leveldb::database::{
+    snapshots::{Snapshot, Snapshots},
+    Database,
+};
 use leveldb::iterator::Iterable;
 use leveldb::iterator::LevelDBIterator;
 use leveldb::kv::KV;
@@ -10,6 +13,29 @@ use leveldb::options::{Options, ReadOptions, WriteOptions};
 use std::fs;
 use std::path::Path;
 use tempdir::TempDir;
+
+pub struct ReadOnlyLevelDbKvStore(Database<StringKey>);
+pub struct LevelDbSnapshot<'a>(Snapshot<'a, StringKey>);
+impl ReadOnlyLevelDbKvStore {
+    pub fn read_only(
+        path: &Path,
+        cache_size: usize,
+    ) -> Result<ReadOnlyLevelDbKvStore, KvStoreError> {
+        let link_dir = TempDir::new("bazuka_mirror")?.into_path();
+        for p in std::fs::read_dir(path)? {
+            let p = p?;
+            if !p.file_name().eq_ignore_ascii_case("lock") {
+                std::os::unix::fs::symlink(p.path(), link_dir.join(p.file_name()))?;
+            }
+        }
+        let mut options = Options::new();
+        options.cache = Some(Cache::new(cache_size));
+        Ok(ReadOnlyLevelDbKvStore(Database::open(&link_dir, options)?))
+    }
+    pub fn snapshot(&self) -> LevelDbSnapshot {
+        LevelDbSnapshot(self.0.snapshot())
+    }
+}
 
 pub struct LevelDbKvStore(Database<StringKey>);
 impl LevelDbKvStore {
@@ -19,16 +45,6 @@ impl LevelDbKvStore {
         options.create_if_missing = true;
         options.cache = Some(Cache::new(cache_size));
         Ok(LevelDbKvStore(Database::open(path, options)?))
-    }
-    pub fn read_only(path: &Path) -> Result<LevelDbKvStore, KvStoreError> {
-        let link_dir = TempDir::new("bazuka_mirror")?.into_path();
-        for p in std::fs::read_dir(path)? {
-            let p = p?;
-            if !p.file_name().eq_ignore_ascii_case("lock") {
-                std::os::unix::fs::symlink(p.path(), link_dir.join(p.file_name()))?;
-            }
-        }
-        LevelDbKvStore::new(&link_dir, 64)
     }
 }
 
@@ -53,6 +69,29 @@ impl KvStore for LevelDbKvStore {
             Ok(_) => Ok(()),
             Err(_) => Err(KvStoreError::Failure),
         }
+    }
+    fn pairs(&self, prefix: StringKey) -> Result<HashMap<StringKey, Blob>, KvStoreError> {
+        let it = self.0.iter(ReadOptions::new());
+        it.seek(&prefix);
+        Ok(it
+            .collect::<Vec<_>>()
+            .into_iter()
+            .take_while(|(k, _)| k.0.starts_with(&prefix.0))
+            .map(|(k, v)| (k, Blob(v)))
+            .collect())
+    }
+}
+
+impl<'a> KvStore for LevelDbSnapshot<'a> {
+    fn get(&self, k: StringKey) -> Result<Option<Blob>, KvStoreError> {
+        let read_opts = ReadOptions::new();
+        match self.0.get(read_opts, k) {
+            Ok(v) => Ok(v.map(Blob)),
+            Err(_) => Err(KvStoreError::Failure),
+        }
+    }
+    fn update(&mut self, _: &[WriteOp]) -> Result<(), KvStoreError> {
+        panic!("Cannot update!");
     }
     fn pairs(&self, prefix: StringKey) -> Result<HashMap<StringKey, Blob>, KvStoreError> {
         let it = self.0.iter(ReadOptions::new());
