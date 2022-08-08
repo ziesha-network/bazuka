6bab60909ef2e0a83cb4a4c95b23b09fe6c0789a
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-08-08 14:19:02 +0430

    Get chain info after the rollback

--- src/node/heartbeat/sync_state.rs
+++ src/node/heartbeat/sync_state.rs
@@ -8,31 +8,34 @@ pub async fn sync_state<B: Blockchain>(
     let net = ctx.outgoing.clone();
 
     let ts = ctx.network_timestamp();
-    let height = ctx.blockchain.get_height()?;
-    let last_header = ctx.blockchain.get_tip()?;
+
     let outdated_heights = ctx.blockchain.get_outdated_heights()?;
     if !outdated_heights.is_empty() && ctx.outdated_since.is_none() {
         ctx.outdated_since = Some(ts);
     } else if outdated_heights.is_empty() && ctx.outdated_since.is_some() {
         ctx.outdated_since = None;
     }
-    // Find clients which their height is equal with our height
-    let same_height_peers = ctx
-        .active_peers()
-        .into_iter()
-        .filter(|p| p.info.as_ref().map(|i| i.height == height).unwrap_or(false));
 
     if !outdated_heights.is_empty() {
         if let Some(outdated_since) = ctx.outdated_since {
             if (ts as i64 - outdated_since as i64) > ctx.opts.outdated_heights_threshold as i64 {
-                ctx.banned_headers.insert(last_header, ts);
+                let tip = ctx.blockchain.get_tip()?;
+                ctx.banned_headers.insert(tip, ts);
                 ctx.blockchain.rollback()?;
                 ctx.outdated_since = None;
                 return Ok(());
             }
         }
+        let height = ctx.blockchain.get_height()?;
+        let last_header = ctx.blockchain.get_tip()?;
 
+        // Find clients which their height is equal with our height
+        let same_height_peers = ctx
+            .active_peers()
+            .into_iter()
+            .filter(|p| p.info.as_ref().map(|i| i.height == height).unwrap_or(false));
         drop(ctx);
+
         for peer in same_height_peers {
             let patch = net
                 .bincode_get::<GetStatesRequest, GetStatesResponse>(
