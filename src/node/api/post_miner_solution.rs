93fe85cd8127536b3de9ee75d130479facb18113
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-11-08 14:27:59 +0330

    Don't return err when an expired solution is submitted

--- src/node/api/post_miner_solution.rs
+++ src/node/api/post_miner_solution.rs
@@ -16,34 +16,33 @@ pub async fn post_miner_solution<B: Blockchain>(
 
     let mut nonce_bytes = [0u8; 8];
     nonce_bytes.copy_from_slice(&hex::decode(req.nonce).unwrap());
-    let (mut draft, _) = context
-        .miner_puzzle
-        .as_ref()
-        .ok_or(NodeError::NoCurrentlyMiningBlockError)?
-        .clone();
-    draft.block.header.proof_of_work.nonce = u64::from_le_bytes(nonce_bytes);
-    if context
-        .blockchain
-        .extend(draft.block.header.number, &[draft.block.clone()])
-        .is_ok()
-    {
-        context.on_update()?;
-        let _ = context.blockchain.update_states(&draft.patch.clone());
+    if let Some((mut draft, _)) = context.miner_puzzle.clone() {
+        draft.block.header.proof_of_work.nonce = u64::from_le_bytes(nonce_bytes);
+        if context
+            .blockchain
+            .extend(draft.block.header.number, &[draft.block.clone()])
+            .is_ok()
+        {
+            context.on_update()?;
+            let _ = context.blockchain.update_states(&draft.patch.clone());
 
-        let peer_addresses = context.peer_manager.random_peers(context.opts.num_peers);
-        http::group_request(&peer_addresses, |peer| {
-            net.bincode_post::<PostBlockRequest, PostBlockResponse>(
-                format!("http://{}/bincode/blocks", peer.address),
-                PostBlockRequest {
-                    block: draft.block.clone(),
-                    patch: draft.patch.clone(),
-                },
-                Limit::default().size(1 * KB).time(3 * SECOND),
-            )
-        })
-        .await;
+            let peer_addresses = context.peer_manager.random_peers(context.opts.num_peers);
+            http::group_request(&peer_addresses, |peer| {
+                net.bincode_post::<PostBlockRequest, PostBlockResponse>(
+                    format!("http://{}/bincode/blocks", peer.address),
+                    PostBlockRequest {
+                        block: draft.block.clone(),
+                        patch: draft.patch.clone(),
+                    },
+                    Limit::default().size(1 * KB).time(3 * SECOND),
+                )
+            })
+            .await;
 
-        context.miner_puzzle = None;
+            context.miner_puzzle = None;
+        }
+    } else {
+        log::info!("No block is currently being mined!");
     }
     Ok(PostMinerSolutionResponse {})
 }
