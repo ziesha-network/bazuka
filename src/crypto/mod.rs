ac0aa2a7e9bcd7bbb5b0a47f2f45a1082625c304
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-01-29 00:10:10 +0330

    Add schnorrkel VRF

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -2,6 +2,7 @@ use std::fmt::{Debug, Display};
 use std::str::FromStr;
 
 use crate::zk;
+use rand::{CryptoRng, RngCore};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
@@ -9,6 +10,7 @@ pub mod merkle;
 
 pub mod ed25519;
 pub mod jubjub;
+pub mod vrf;
 
 pub trait SignatureScheme: Clone + Serialize {
     type PubParseError;
@@ -49,4 +51,15 @@ pub trait ZkSignatureScheme: Clone + Serialize {
     fn generate_keys(seed: &[u8]) -> (Self::Pub, Self::Priv);
     fn sign(sk: &Self::Priv, msg: zk::ZkScalar) -> Self::Sig;
     fn verify(pk: &Self::Pub, msg: zk::ZkScalar, sig: &Self::Sig) -> bool;
+}
+
+pub trait VerifiableRandomFunction: Clone + Serialize {
+    type Ctx;
+    type Pub;
+    type Priv;
+    type Sig;
+    fn make_context(bytes: &[u8]) -> Self::Ctx;
+    fn generate_keys<R: RngCore + CryptoRng>(csprng: R) -> (Self::Pub, Self::Priv);
+    fn sign(sk: &Self::Priv, ctx: &Self::Ctx, msg: &[u8]) -> Self::Sig;
+    fn verify(pk: &Self::Pub, ctx: &Self::Ctx, msg: &[u8], sig: &Self::Sig) -> bool;
 }
