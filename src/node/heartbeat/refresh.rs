681cb9e39a90589870a13fc41459395bc591188c
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-01-30 23:55:36 +0330

    Keep a fixed number of peers

--- src/node/heartbeat/refresh.rs
+++ src/node/heartbeat/refresh.rs
@@ -2,6 +2,10 @@ use super::*;
 
 pub async fn refresh<B: Blockchain>(context: Arc<RwLock<NodeContext<B>>>) -> Result<(), NodeError> {
     let mut ctx = context.write().await;
+    let opts = ctx.opts.clone();
     ctx.refresh()?;
+    if ctx.peer_manager.get_peers().len() < opts.num_peers {
+        ctx.peer_manager.select_peers(opts.num_peers);
+    }
     Ok(())
 }
