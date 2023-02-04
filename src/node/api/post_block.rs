e49a6be18e4552f8d9a5750f91e4a13252ab59d8
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-05 03:27:49 +0330

    Propagate blocks to the peers

--- src/node/api/post_block.rs
+++ src/node/api/post_block.rs
@@ -1,6 +1,7 @@
 use super::messages::{PostBlockRequest, PostBlockResponse};
-use super::{NodeContext, NodeError};
+use super::{http, Limit, NodeContext, NodeError};
 use crate::blockchain::Blockchain;
+use crate::common::*;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -9,10 +10,24 @@ pub async fn post_block<B: Blockchain>(
     req: PostBlockRequest,
 ) -> Result<PostBlockResponse, NodeError> {
     let mut context = context.write().await;
-    context
-        .blockchain
-        .extend(req.block.header.number, &[req.block])?;
-    context.on_update()?;
-    context.blockchain.update_states(&req.patch)?;
+    if req.block.header.number == context.blockchain.get_height()? {
+        context
+            .blockchain
+            .extend(req.block.header.number, &[req.block.clone()])?;
+        context.on_update()?;
+        context.blockchain.update_states(&req.patch)?;
+        let net = context.outgoing.clone();
+        let peer_addresses = context.peer_manager.get_peers();
+        drop(context);
+
+        http::group_request(&peer_addresses, |peer| {
+            net.bincode_post::<PostBlockRequest, PostBlockResponse>(
+                format!("http://{}/bincode/blocks", peer.address),
+                req.clone(),
+                Limit::default().size(KB).time(3 * SECOND),
+            )
+        })
+        .await;
+    }
     Ok(PostBlockResponse {})
 }
