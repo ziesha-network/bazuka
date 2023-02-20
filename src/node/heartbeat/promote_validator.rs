50e13fdd964faecc3777839d1be830f715c237c4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-21 01:30:42 +0330

    Only promote validator once and nullify claim when period is over

--- src/node/heartbeat/promote_validator.rs
+++ src/node/heartbeat/promote_validator.rs
@@ -6,12 +6,20 @@ pub async fn promote_validator<B: Blockchain>(
     let mut ctx = context.write().await;
     let timestamp = ctx.network_timestamp();
     if let Some(proof) = ctx.blockchain.validator_status(timestamp, &ctx.wallet)? {
+        if let Some(claim) = &ctx.validator_claim {
+            if claim.address == ctx.wallet.get_address() {
+                return Ok(());
+            }
+        }
         println!("You are the validator! Promoting...");
         let node = ctx.address.ok_or(NodeError::ValidatorNotExposed)?;
         let claim = ctx.wallet.claim_validator(timestamp, proof, node);
         ctx.validator_claim = Some(claim.clone());
         drop(ctx);
         promote_validator_claim(context, claim).await;
+    } else {
+        println!("You are not the validator anymore!");
+        ctx.validator_claim = None;
     }
     Ok(())
 }
