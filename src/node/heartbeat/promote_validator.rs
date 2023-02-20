9bffbaa408d8980958f5575a7a6d59f98df1997a
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-21 01:27:00 +0330

    Log when promoting validator

--- src/node/heartbeat/promote_validator.rs
+++ src/node/heartbeat/promote_validator.rs
@@ -6,6 +6,7 @@ pub async fn promote_validator<B: Blockchain>(
     let mut ctx = context.write().await;
     let timestamp = ctx.network_timestamp();
     if let Some(proof) = ctx.blockchain.validator_status(timestamp, &ctx.wallet)? {
+        println!("You are the validator! Promoting...");
         let node = ctx.address.ok_or(NodeError::ValidatorNotExposed)?;
         let claim = ctx.wallet.claim_validator(timestamp, proof, node);
         ctx.validator_claim = Some(claim.clone());
