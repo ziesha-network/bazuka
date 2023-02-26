fb592238a653ef3eba901445977b017e52fcd495
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-26 19:21:04 +0330

    Fix compile bug

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -723,7 +723,7 @@ impl<K: KvStore> KvStoreChain<K> {
                     since,
                     count,
                 } => {
-                    if count > self.config.max_epoch_delegate {
+                    if *count > self.config.max_epoch_delegate {
                         return Err(BlockchainError::DelegateExcessiveEpochCount);
                     }
                     let mut src_bal = chain.get_balance(tx_src.clone(), TokenId::Ziesha)?;
