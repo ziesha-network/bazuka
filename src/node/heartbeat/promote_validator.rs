0ce8e9f06d38ba82231f26a8bfc7a54bccb67809
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-03-03 19:44:10 +0330

    Refactor validator-promotion heartbeat func

--- src/node/heartbeat/promote_validator.rs
+++ src/node/heartbeat/promote_validator.rs
@@ -7,23 +7,16 @@ pub async fn promote_validator<B: Blockchain>(
     let timestamp = ctx.network_timestamp();
     let proof = ctx.blockchain.validator_status(timestamp, &ctx.wallet)?;
     if !proof.is_unproven() {
-        println!("You are the validator! Promoting...");
-        if let Some(claim) = ctx.validator_claim.clone() {
-            let (curr_epoch, curr_slot) = ctx.blockchain.epoch_slot(timestamp);
-            let (claim_epoch, claim_slot) = ctx.blockchain.epoch_slot(claim.timestamp);
-            let same_epoch_slot = curr_epoch == claim_epoch && curr_slot == claim_slot;
-            if claim.address == ctx.wallet.get_address() && same_epoch_slot {
-                drop(ctx);
-                promote_validator_claim(context, claim).await;
-                return Ok(());
-            }
-        }
-
         let node = ctx.address.ok_or(NodeError::ValidatorNotExposed)?;
         let claim = ctx.wallet.claim_validator(timestamp, proof, node);
-        ctx.validator_claim = Some(claim.clone());
-        drop(ctx);
-        promote_validator_claim(context, claim).await;
+        ctx.update_validator_claim(claim.clone())?;
+        if let Some(claim) = ctx.validator_claim.clone() {
+            println!("You are the validator! Promoting...");
+            if claim.address == ctx.wallet.get_address() {
+                drop(ctx);
+                promote_validator_claim(context, claim).await;
+            }
+        }
     } else {
         ctx.mpn_work_pool = None;
         if let Some(claim) = ctx.validator_claim.clone() {
