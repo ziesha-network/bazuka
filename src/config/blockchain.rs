76434fe344cb240cce1ae5010f138f13c1d184b2
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-03-06 18:12:47 +0330

    More organized MPN configurations

--- src/config/blockchain.rs
+++ src/config/blockchain.rs
@@ -14,9 +14,7 @@ use crate::wallet::TxBuilder;
 
 const CHAIN_START_TIMESTAMP: u32 = 1677770301 - 60 * 5;
 
-const MPN_LOG4_ACCOUNT_CAPACITY: u8 = 15;
-const MPN_LOG4_PAYMENT_CAPACITY: u8 = 3;
-const MPN_LOG4_UPDATE_CAPACITY: u8 = 4;
+const MPN_LOG4_TREE_SIZE: u8 = 15;
 const MPN_LOG4_TOKENS_TREE_SIZE: u8 = 3;
 const MPN_LOG4_DEPOSIT_BATCH_SIZE: u8 = 3;
 const MPN_LOG4_WITHDRAW_BATCH_SIZE: u8 = 3;
@@ -34,16 +32,20 @@ lazy_static! {
         bincode::deserialize(&hex::decode("b02701884fb4065e5dec5456f29cbbf7b093b5847c56b7f6c1fb103851b674f9122395c01b2ac3015bbffddd0ccce114a8c239c56aa3543ba593e69f94a411230b6138bbfade4ac527e990466b1b625617f415f58d572e2b0f559e590180ee17005001160b651af92d477bc900a6f468abe5a03d8d16667e104721d84053149b8c8e6dbaaa04f767fe3480adf9ec4e2501948c01cd4d17416f97407c9b1b69bd004dbeefb3ab8a56893eb0efd44d13f740d479eb3b43d4b11b0e23f9bed985ac0a0033316f8dbcea7ba33a2e6e3225c09f3db359b808dcd316f27ac309886060cda95c63b1f274d2f15731dd2e54027173182b5f79b1b1875c11669b2a89584308f461ce1becda321c0ede1c8e060e3dea7255d464c93ce846d65d200327888a320043ba1a5d14a41af8c158ed640c8d3ea06a21525671261fd03f8050c6e25c643a6dfb27418d1b36c14c3ce4a035b22a07a70b43b2f39e4cc54ff9bcc27f36508f0a408446d47a5e520c14a809605865a074631777ba098eb61145839216fa571000c0bd67354bcfaff0ac9be6d6e60dd27ba907b73e48cd29c9d04bfb1648047d00e6e8357101d30b79946c6072c6967909b9aae7f069033cafaea578a6b2e0e6b2bfabd528e90c2d3424af26a7d26bf95dd06296c89ddd8a662c52756656304118dce1cb5ca358fa9726344e8c37eede52e11786758be88dab87d896216dc0291c8f250322ca0aff90cea90f5ac30a250a65e187464f11b76f15fe8fd5ae1a71fd02131af2f1585807ba1729693d7481ec47d7731eefba89272466472f6482d109004188bba4fc60efa79ea39994af0bf56accd370b06fdce321aa7c0d00d4bf8cfac3ef3408822145f58963bcddd84f1711752f24db6810bcfc10b9f2d1ee7601703e2da6f8c42ce2e771e85dc81f0f71d3ec1537848e1d29220136e4193ae98a17005732f4779ecd296857e4217453314ebc5b733d289cae7d2b4109ac8df4d7cf4b368c6942006c79503155fde7a4dfbd1840f3f8f8599dcfded2050bfc1c1f41d9a0931b52bd5ea22053e7913104eba04a68e4aa9991c74949ca80871c14744f0c247b6df3c6bd961430f1aa53b855967a91432ad5645876e6b67ccf29f0cb6b2197bdc3fae24a8f5c5215aab931e62b193e64b49c48f4e916a73a2752542b78c53b7b96ab8a819fd45c37c6bc5ef76fe5b7a1d8f74df6a776b413bb7bd3852803000500000000000000f85d7a9418cef172bfb155561966fb1978f4a27569f2187964d93828824ea453ef131a512eaa4292eec5fb9069183110ccb6d2300144d5a89c3d85b7eb481272442678354671e88e46df7b0b1b1aeab06187c12abe7bda331952fafddee51a1300ca993bfef2e7a6680b3d9f35e073b3a9951f6d191f44436db4edd1f413fbb54eba5713d395987ccc937a0b646b21c802236b88d3658fa4dbf7cfdde6abd0c56f936753deb02ca4ca4ea6f57ba53e3228c7f9b14c044934fca0b2a513fe3ea510004b710575e4e79f71970e7a88f4935a34cea42bfd0a415b78557c1664e0e53e14eb156a7dbac5c83a88264c2501f53315a462194bfaba4125e2870f4f2dd6a4f1fecb22173ec1b82db95cd6c849bcf3e6d490523c1ce9ec15ca9364d01647430600ce6ee3547b59cb629b43a2afeadce961aaf1003cba71172c96d8eb8d96d4548779a87b06fcc3e4d78c079da529e944186335d815301862bbd3eaf22e1e1e3bfa387d5e40f1e7336facd285be3cf9e02801f061748477c7071e34a759c3ef661800fd51c582d128a1f4d508e37d02caa492f5726bceed6ede47de19e4134dec5515da51b7255779ad2a27ef58883043e80fb0e22eaac9598d142438a82678a3bc918495c86e5d64575c6a732ac2bf14295a0f42faa55fdd25f23510c8e3f7125f0c00").unwrap()).unwrap();
 }
 
-fn get_mpn_contract() -> TransactionAndDelta {
+fn get_mpn_contract(
+    log4_token_tree_size: u8,
+    log4_deposit_batch_size: u8,
+    log4_withdraw_batch_size: u8,
+) -> TransactionAndDelta {
     let mpn_state_model = zk::ZkStateModel::List {
-        log4_size: MPN_LOG4_ACCOUNT_CAPACITY,
+        log4_size: MPN_LOG4_TREE_SIZE,
         item_type: Box::new(zk::ZkStateModel::Struct {
             field_types: vec![
                 zk::ZkStateModel::Scalar, // Nonce
                 zk::ZkStateModel::Scalar, // Pub-key X
                 zk::ZkStateModel::Scalar, // Pub-key Y
                 zk::ZkStateModel::List {
-                    log4_size: MPN_LOG4_PAYMENT_CAPACITY,
+                    log4_size: log4_token_tree_size,
                     item_type: Box::new(zk::ZkStateModel::Struct {
                         field_types: vec![
                             zk::ZkStateModel::Scalar, // Token-Id
@@ -59,11 +61,11 @@ fn get_mpn_contract() -> TransactionAndDelta {
         initial_state: zk::ZkCompressedState::empty::<ZkHasher>(mpn_state_model),
         deposit_functions: vec![zk::ZkMultiInputVerifierKey {
             verifier_key: zk::ZkVerifierKey::Groth16(Box::new(MPN_DEPOSIT_VK.clone())),
-            log4_payment_capacity: MPN_LOG4_PAYMENT_CAPACITY,
+            log4_payment_capacity: log4_deposit_batch_size,
         }],
         withdraw_functions: vec![zk::ZkMultiInputVerifierKey {
             verifier_key: zk::ZkVerifierKey::Groth16(Box::new(MPN_WITHDRAW_VK.clone())),
-            log4_payment_capacity: MPN_LOG4_PAYMENT_CAPACITY,
+            log4_payment_capacity: log4_withdraw_batch_size,
         }],
         functions: vec![zk::ZkSingleInputVerifierKey {
             verifier_key: zk::ZkVerifierKey::Groth16(Box::new(MPN_UPDATE_VK.clone())),
@@ -87,7 +89,7 @@ fn get_mpn_contract() -> TransactionAndDelta {
 
 #[cfg(test)]
 fn get_test_mpn_contract() -> TransactionAndDelta {
-    let mut mpn_tx_delta = get_mpn_contract();
+    let mut mpn_tx_delta = get_mpn_contract(1, 1, 1);
     let init_state = zk::ZkDataPairs(
         [(zk::ZkDataLocator(vec![100]), zk::ZkScalar::from(200))]
             .into_iter()
@@ -141,7 +143,11 @@ fn get_ziesha_token_creation_tx() -> Transaction {
 }
 
 pub fn get_blockchain_config() -> BlockchainConfig {
-    let mpn_tx_delta = get_mpn_contract();
+    let mpn_tx_delta = get_mpn_contract(
+        MPN_LOG4_TOKENS_TREE_SIZE,
+        MPN_LOG4_DEPOSIT_BATCH_SIZE,
+        MPN_LOG4_WITHDRAW_BATCH_SIZE,
+    );
     let mpn_contract_id = ContractId::new(&mpn_tx_delta.tx);
 
     let ziesha_token_creation_tx = get_ziesha_token_creation_tx();
@@ -201,11 +207,11 @@ pub fn get_blockchain_config() -> BlockchainConfig {
         limited_miners: None,
         mpn_config: MpnConfig {
             mpn_contract_id,
-            log4_tree_size: MPN_LOG4_ACCOUNT_CAPACITY,
+            log4_tree_size: MPN_LOG4_TREE_SIZE,
             log4_token_tree_size: MPN_LOG4_TOKENS_TREE_SIZE,
             log4_deposit_batch_size: MPN_LOG4_DEPOSIT_BATCH_SIZE,
             log4_withdraw_batch_size: MPN_LOG4_WITHDRAW_BATCH_SIZE,
-            log4_update_batch_size: MPN_LOG4_UPDATE_CAPACITY,
+            log4_update_batch_size: MPN_LOG4_UPDATE_BATCH_SIZE,
             mpn_num_update_batches: 1,
             mpn_num_deposit_batches: 1,
             mpn_num_withdraw_batches: 1,
