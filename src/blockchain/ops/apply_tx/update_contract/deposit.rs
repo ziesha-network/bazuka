0f946965bf1080e58e38857463bb85f02a011a4b
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-03-25 02:23:54 +0430

    Test deposit of custom-tokens

--- src/blockchain/ops/apply_tx/update_contract/deposit.rs
+++ src/blockchain/ops/apply_tx/update_contract/deposit.rs
@@ -108,20 +108,6 @@ mod tests {
         )
         .unwrap();
 
-        let deposit_state_model = zk::ZkStateModel::List {
-            item_type: Box::new(zk::ZkStateModel::Struct {
-                field_types: vec![
-                    zk::ZkStateModel::Scalar, // Enabled
-                    zk::ZkStateModel::Scalar, // Token-id
-                    zk::ZkStateModel::Scalar, // Amount
-                    zk::ZkStateModel::Scalar, // Calldata
-                ],
-            }),
-            log4_size: 1,
-        };
-        let empty_compressed =
-            zk::ZkCompressedState::empty::<crate::core::ZkHasher>(deposit_state_model);
-
         let contract_id = chain.config.mpn_config.mpn_contract_id;
 
         let abc = TxBuilder::new(&Vec::from("ABC"));
@@ -199,6 +185,116 @@ mod tests {
             vec![Money {
                 token_id: TokenId::Ziesha,
                 amount: Amount(77)
+            }]
+        );
+    }
+
+    #[test]
+    fn test_custom_token_deposit() {
+        let mut chain = KvStoreChain::new(
+            RamKvStore::new(),
+            crate::config::blockchain::get_test_blockchain_config(),
+        )
+        .unwrap();
+
+        let contract_id = chain.config.mpn_config.mpn_contract_id;
+
+        let abc = TxBuilder::new(&Vec::from("ABC"));
+        let (tx_delta, kiwi_token_id) = abc.create_token(
+            "".into(),
+            "KeyvanCoin".into(),
+            "KIWI".into(),
+            Amount(100000),
+            3,
+            None,
+            Money::ziesha(0),
+            1,
+        );
+        chain.apply_tx(&tx_delta.tx, false).unwrap();
+
+        let abc = TxBuilder::new(&Vec::from("ABC"));
+        let mut cont_deposit = ContractDeposit {
+            memo: "".into(),
+            src: abc.get_address(),
+            contract_id,
+            deposit_circuit_id: 0,
+            calldata: zk::ZkScalar::from(888),
+            nonce: 2,
+            amount: Money {
+                token_id: kiwi_token_id,
+                amount: Amount(10000),
+            },
+            fee: Money::ziesha(321),
+            sig: None,
+        };
+        abc.sign_deposit(&mut cont_deposit);
+
+        let (ops, ((_, aux_data), exec_fees)) = chain
+            .isolated(|chain| {
+                let mut exec_fees = Vec::new();
+                let contract = chain.get_contract(contract_id)?;
+                Ok((
+                    deposit(
+                        chain,
+                        &contract_id,
+                        &contract,
+                        &0,
+                        &[cont_deposit],
+                        &mut exec_fees,
+                    )?,
+                    exec_fees,
+                ))
+            })
+            .unwrap();
+        let empty_leaf =
+            <crate::core::ZkHasher as crate::zk::ZkHasher>::hash(&[zk::ZkScalar::from(0); 4]);
+        let expected_hash = <crate::core::ZkHasher as crate::zk::ZkHasher>::hash(&[
+            <crate::core::ZkHasher as crate::zk::ZkHasher>::hash(&[
+                zk::ZkScalar::from(1),
+                zk::ZkScalar::from(kiwi_token_id),
+                zk::ZkScalar::from(10000),
+                zk::ZkScalar::from(888),
+            ]),
+            empty_leaf,
+            empty_leaf,
+            empty_leaf,
+        ]);
+
+        let expected_aux = zk::ZkCompressedState {
+            state_hash: expected_hash,
+            state_size: 4,
+        };
+
+        assert_eq!(aux_data, expected_aux);
+
+        let expected_ops = vec![
+            WriteOp::Put(
+                "ACB-ed8c19c6a4cf1460e961f7bae8eea54d437b9edac27cbeb09be32ae367adf9098a-0x113c5707d6ab03dd78c9f8455f3b4096c8c5e9312076aafe38d79c4d8eb2d096"
+                    .into(),
+                Amount(90000).into(),
+            ),
+            WriteOp::Put(
+                "ACB-ed8c19c6a4cf1460e961f7bae8eea54d437b9edac27cbeb09be32ae367adf9098a-Ziesha"
+                    .into(),
+                Amount(9679).into(),
+            ),
+            WriteOp::Put(
+                "ACC-ed8c19c6a4cf1460e961f7bae8eea54d437b9edac27cbeb09be32ae367adf9098a".into(),
+                Account { nonce: 2 }.into(),
+            ),
+            WriteOp::Put(
+                "CAB-1525ced32cb40609838e7dad549014268e4449abc2a1621e485bc8f88a48f223-0x113c5707d6ab03dd78c9f8455f3b4096c8c5e9312076aafe38d79c4d8eb2d096"
+                    .into(),
+                Amount(10000).into(),
+            ),
+        ];
+
+        assert_eq!(ops, expected_ops);
+        assert_eq!(
+            exec_fees,
+            vec![Money {
+                token_id: TokenId::Ziesha,
+                amount: Amount(321)
             }]
         );
     }
