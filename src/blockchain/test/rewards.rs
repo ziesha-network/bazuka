97644104ca188b15689e195e4bda8b4a5dc97173
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-04-15 01:30:33 +0430

    Test if auto-delegation works

--- src/blockchain/test/rewards.rs
+++ src/blockchain/test/rewards.rs
@@ -117,3 +117,72 @@ fn test_correct_rewards() {
         Amount((expected_reward_3 - expected_validator_reward_3).0 * 3 / 5)
     ));
 }
+
+#[test]
+fn test_auto_delegate() {
+    let validator = TxBuilder::new(&Vec::from("VALIDATOR"));
+    let delegator = TxBuilder::new(&Vec::from("DELEGATOR"));
+    let mut chain = KvStoreChain::new(
+        db::RamKvStore::new(),
+        blockchain::get_test_blockchain_config(),
+    )
+    .unwrap();
+
+    chain
+        .apply_tx(
+            &Transaction {
+                memo: "".into(),
+                src: Some(delegator.get_address()),
+                data: TransactionData::AutoDelegate {
+                    to: validator.get_address(),
+                    ratio: Ratio(55),
+                },
+                nonce: 0,
+                fee: Money::ziesha(0),
+                sig: Signature::Unsigned,
+            },
+            true,
+        )
+        .unwrap();
+
+    let expected_reward = chain.next_reward().unwrap();
+    let expected_validator_reward = Amount(expected_reward.0 * 12 / 255);
+    let expected_delegator_reward_before_auto_delegate =
+        expected_reward - expected_validator_reward;
+    let expected_delegator_reward_after_auto_delegate =
+        Amount((expected_delegator_reward_before_auto_delegate.0 as f64 * 200.0 / 255.0) as u64);
+    let expected_delegator_balance = Amount(25) + expected_delegator_reward_after_auto_delegate;
+    let expected_delegation = Amount(25)
+        + Amount((expected_delegator_reward_before_auto_delegate.0 as f64 * 55.0 / 255.0) as u64);
+    assert_eq!(
+        chain
+            .get_balance(delegator.get_address(), TokenId::Ziesha)
+            .unwrap(),
+        Amount(25)
+    );
+    assert_eq!(expected_reward, Amount(19999999999999));
+    let draft = chain
+        .draft_block(0, &[], &validator, true)
+        .unwrap()
+        .unwrap();
+    chain.apply_block(&draft.block).unwrap();
+    assert!(close_enough(
+        chain
+            .get_delegate(delegator.get_address(), validator.get_address())
+            .unwrap()
+            .amount,
+        expected_delegation
+    ));
+    assert!(close_enough(
+        chain
+            .get_balance(validator.get_address(), TokenId::Ziesha)
+            .unwrap(),
+        expected_validator_reward
+    ));
+    assert!(close_enough(
+        chain
+            .get_balance(delegator.get_address(), TokenId::Ziesha)
+            .unwrap(),
+        expected_delegator_balance
+    ));
+}
