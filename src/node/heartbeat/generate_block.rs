4628021ad14fdffb0fa94c7c5afdaff44eb86108
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-06 16:28:18 +0430

    Promote validator before generating mpn block

--- src/node/heartbeat/generate_block.rs
+++ src/node/heartbeat/generate_block.rs
@@ -10,6 +10,7 @@ pub async fn generate_block<K: KvStore, B: Blockchain<K>>(
     let proof = ctx
         .blockchain
         .validator_status(timestamp, &ctx.validator_wallet)?;
+
     if !proof.is_unproven() {
         let (tip_epoch, tip_slot) = ctx
             .blockchain
@@ -19,6 +20,18 @@ pub async fn generate_block<K: KvStore, B: Blockchain<K>>(
             return Ok(());
         }
 
+        let node = ctx.address.ok_or(NodeError::ValidatorNotExposed)?;
+        let claim = ctx.validator_wallet.claim_validator(timestamp, proof, node);
+        let is_new_claim = ctx.update_validator_claim(claim.clone())?;
+        if let Some(claim) = ctx.validator_claim.clone() {
+            println!("You are the validator! Promoting...");
+            if claim.address == ctx.validator_wallet.get_address() {
+                drop(ctx);
+                promote_validator_claim(context.clone(), claim).await;
+            }
+        }
+
+        let mut ctx = context.write().await;
         if ctx.opts.automatic_block_generation {
             if let Some(work_pool) = &ctx.mpn_work_pool {
                 let wallet = ctx.validator_wallet.clone();
@@ -30,66 +43,57 @@ pub async fn generate_block<K: KvStore, B: Blockchain<K>>(
                         ctx.mpn_work_pool = None;
                         ctx.validator_claim = None;
                         drop(ctx);
-                        promote_block(context, draft).await;
+                        promote_block(context.clone(), draft).await;
                         return Ok(());
                     }
                 }
             }
-        }
-        let node = ctx.address.ok_or(NodeError::ValidatorNotExposed)?;
-        let claim = ctx.validator_wallet.claim_validator(timestamp, proof, node);
-        if ctx.update_validator_claim(claim.clone())? {
-            let mempool = ctx.mempool.clone();
+            if is_new_claim {
+                let mempool = ctx.mempool.clone();
 
-            let updates = mempool
-                .mpn_txs()
-                .map(|(tx, _)| tx.clone())
-                .collect::<Vec<_>>();
-            let deposits = mempool
-                .mpn_deposits()
-                .map(|(tx, _)| tx.clone())
-                .collect::<Vec<_>>();
-            let withdraws = mempool
-                .mpn_withdraws()
-                .map(|(tx, _)| tx.clone())
-                .collect::<Vec<_>>();
+                let updates = mempool
+                    .mpn_txs()
+                    .map(|(tx, _)| tx.clone())
+                    .collect::<Vec<_>>();
+                let deposits = mempool
+                    .mpn_deposits()
+                    .map(|(tx, _)| tx.clone())
+                    .collect::<Vec<_>>();
+                let withdraws = mempool
+                    .mpn_withdraws()
+                    .map(|(tx, _)| tx.clone())
+                    .collect::<Vec<_>>();
 
-            let validator_reward = ctx
-                .blockchain
-                .min_validator_reward(ctx.validator_wallet.get_address())?;
+                let validator_reward = ctx
+                    .blockchain
+                    .min_validator_reward(ctx.validator_wallet.get_address())?;
 
-            let deposit_nonce = ctx.blockchain.get_deposit_nonce(
-                ctx.validator_wallet.get_address(),
-                ctx.blockchain.config().mpn_config.mpn_contract_id,
-            )?;
-            let mpn_nonce = ctx
-                .blockchain
-                .get_mpn_account(MpnAddress {
-                    pub_key: ctx.validator_wallet.get_zk_address(),
-                })?
-                .tx_nonce;
-            ctx.mpn_work_pool = Some(mpn::prepare_works(
-                &ctx.blockchain.config().mpn_config,
-                &ctx.blockchain,
-                &ctx.mpn_workers,
-                deposits,
-                withdraws,
-                updates,
-                validator_reward,
-                Amount(100_000_000_000), // TODO: Remove Hardcoded rewards
-                Amount(100_000_000_000),
-                Amount(300_000_000_000),
-                deposit_nonce,
-                mpn_nonce,
-                ctx.validator_wallet.clone(),
-                ctx.user_wallet.clone(),
-            )?);
-        }
-        if let Some(claim) = ctx.validator_claim.clone() {
-            println!("You are the validator! Promoting...");
-            if claim.address == ctx.validator_wallet.get_address() {
-                drop(ctx);
-                promote_validator_claim(context, claim).await;
+                let deposit_nonce = ctx.blockchain.get_deposit_nonce(
+                    ctx.validator_wallet.get_address(),
+                    ctx.blockchain.config().mpn_config.mpn_contract_id,
+                )?;
+                let mpn_nonce = ctx
+                    .blockchain
+                    .get_mpn_account(MpnAddress {
+                        pub_key: ctx.validator_wallet.get_zk_address(),
+                    })?
+                    .tx_nonce;
+                ctx.mpn_work_pool = Some(mpn::prepare_works(
+                    &ctx.blockchain.config().mpn_config,
+                    &ctx.blockchain,
+                    &ctx.mpn_workers,
+                    deposits,
+                    withdraws,
+                    updates,
+                    validator_reward,
+                    Amount(100_000_000_000), // TODO: Remove Hardcoded rewards
+                    Amount(100_000_000_000),
+                    Amount(300_000_000_000),
+                    deposit_nonce,
+                    mpn_nonce,
+                    ctx.validator_wallet.clone(),
+                    ctx.user_wallet.clone(),
+                )?);
             }
         }
     } else {
