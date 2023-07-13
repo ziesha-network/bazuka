ffc0c37e6a371e4ccee8925f974efbb7eeb70ea5
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-07-13 18:00:36 +0430

    Refactor health-check command

--- src/cli/chain/health_check.rs
+++ src/cli/chain/health_check.rs
@@ -26,20 +26,24 @@ pub fn health_check(conf: &BazukaConfig) {
         .into_iter()
         .collect::<Vec<_>>()
         .is_empty();
+    let acc_count = chain.get_mpn_account_count().unwrap();
     let mut sum_mpn: Amount = 0.into();
-    for mpn_acc in chain.get_mpn_accounts(0, 10000).unwrap() {
+    for mpn_acc in chain.get_mpn_accounts(0, acc_count as usize).unwrap() {
         for money in mpn_acc.1.tokens.values() {
             if money.token_id == TokenId::Ziesha {
                 sum_mpn += money.amount;
             }
         }
     }
+
+    let expected_currency_in_circulation = Amount::from(2000000000000000000);
+
     let mpn_contract_balance_check = sum_mpn
         == chain
             .get_contract_balance(mpn_contract_id, TokenId::Ziesha)
             .unwrap();
     let currency_in_circulation_check =
-        chain.currency_in_circulation().unwrap() == Amount::from(2000000000000000000);
+        chain.currency_in_circulation().unwrap() == expected_currency_in_circulation;
     println!(
         "Rollback validity check: {}",
         if rollback_validity_check {
