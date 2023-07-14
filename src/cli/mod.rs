ed8a7ecef7d895149202448abe4f2c5b6d2571e5
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-07-14 15:25:15 +0430

    Continue testnet

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -44,7 +44,7 @@ pub mod node;
 const DEFAULT_PORT: u16 = 8765;
 const BAZUKA_NOT_INITILIZED: &str = "Bazuka is not initialized";
 
-const CURRENT_NETWORK: &str = "deruny-4";
+const CURRENT_NETWORK: &str = "deruny-5";
 
 #[cfg(feature = "client")]
 #[derive(Serialize, Deserialize, Clone, Debug)]
